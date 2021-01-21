0593ea9c8b931f105f2bc8a222953ba1940f68f9
Iqbal Ahmed <iqbalhussain9865@gmail.com>
2021-01-21 23:40:16 +0600

    added second problem

--- assignment.js
+++ assignment.js
@@ -1,5 +1,7 @@
 //https://github.com/iqbal9865/AssignmentThree
 
+
+//start first problem
 function kilometerToMeter(km){
     if(km < 0){
         return 'Please Input a Positive Number';
@@ -11,3 +13,23 @@ function kilometerToMeter(km){
 }
 var distance = kilometerToMeter(7);
 console.log(distance);
+//end first problem
+
+
+
+//start second problem
+function budgetCalculator(numOfWatch,numOfPhone,numOfLaptop){
+    if(numOfWatch < 0 || numOfPhone < 0 || numOfLaptop < 0){
+        return "Invalid Input";
+    }
+    else{
+    var watchAmount = numOfWatch * 50;
+    var phoneAmount = numOfPhone * 100;
+    var laptopAmount = numOfLaptop * 500;
+    var total = watchAmount + phoneAmount + laptopAmount;
+    return total;
+    }
+}
+var totalCost = budgetCalculator(5,4,3);
+console.log(totalCost);
+//end second problem
